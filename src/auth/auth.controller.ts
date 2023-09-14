dcd321fb74be46c1ca69db09faa1c7b6f1b5a559
Lydie Chevalier <didof69@hotmail.fr>
2023-09-14 10:48:27 +0200

    API FULL OK V.2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,24 +18,4 @@ export class AuthController {
   login(@Body() loginDto: LoginDto): Promise<{ accessToken: string }> {
     return this.authService.login(loginDto);
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.authService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.authService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-  //   return this.authService.update(+id, updateAuthDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.authService.remove(+id);
-  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -60,20 +60,4 @@ export class AuthService {
       )
     }
    }
-  
-  // findAll() {
-  //   return `This action returns all auth`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} auth`;
-  // }
-
-  // update(id: number, updateAuthDto: UpdateAuthDto) {
-  //   return `This action updates a #${id} auth`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} auth`;
-  // }
 }
